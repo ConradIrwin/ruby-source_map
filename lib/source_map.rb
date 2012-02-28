0f257531c70d3ee81767f85c6b9978edee328fc6
Conrad Irwin <conrad.irwin@gmail.com>
2012-02-28 13:44:31 -0800

    Clean up initializer

--- lib/source_map.rb
+++ lib/source_map.rb
@@ -4,27 +4,42 @@ require File.expand_path("../source_map/generator.rb", __FILE__)
 class SourceMap
   include SourceMap::Generator
 
+  # Create a new blank SourceMap
+  #
+  # Options may include:
+  #
+  # :file => String         # See {#file}
+  # :source_root => String  # See {#source_root}
+  # :generated_output => IO # See {#generated_output}
+  # :version => 3           # Which version of SourceMap to use (only 3 is allowed)
+  #
   def initialize(opts={})
+    unless (remain = opts.keys - [:generated_output, :file, :source_root, :version]).empty?
+      raise ArgumentError, "Unsupported options to SourceMap.new: #{remain.inspect}"
+    end
     self.generated_output = opts[:generated_output]
     self.file = opts[:file] || ''
     self.source_root = opts[:source_root] || ''
-    raise "version #{opts[:version]} not supported" if opts[:version] && opts[:version] != 3
+    self.version = opts[:version] || 3
+    raise "version #{opts[:version]} not supported" if version != 3
   end
 
-  # The name of the generated file that this map is associated with.
+  # The name of the file containing the code that this SourceMap describes.
   # (default "")
   attr_accessor :file
 
-  # The base path/url to which the given source locations are relative.
+  # The URL/directory that contains the original source files.
+  #
+  # This is prefixed to the entries in ['sources']
   # (default "")
   attr_accessor :source_root
 
-  # The version of the SourceMap spec to which this SourceMap responds.
-  def version
-    3
-  end
+  # The version of the SourceMap spec we're using.
+  # (default 3)
+  attr_accessor :version
 
   # The list of sources (used during parsing/generating)
+  # These are relative to the source_root.
   def sources
     @sources ||= []
   end
@@ -34,7 +49,7 @@ class SourceMap
     @names ||= []
   end
 
-  # A list of mapping objects, see {add_mapping}
+  # A list of mapping objects.
   def mappings
     @mappings ||= []
   end
